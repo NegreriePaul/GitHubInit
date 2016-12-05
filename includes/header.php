3e3dd672ef8ba34fe9b59b85c59ab89fd6eb73f3
gurnav <singh.g@outlook.fr>
2016-12-05 11:30:10 +0100

    ajout header et footer du site

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+    <header>
+        <div class="title_header">
+            <a href="#to_top_id" class="to_top"><img src="data/images/gmh_logo.png" id="logo_img"></a>
+                 <div class="account">
+                    <ul id="social">
+                        <li><a target="_blank" href=""><img src="data/images/social/mail.png"></a></li>
+                        <li><a target="_blank" href=""><img src="data/images/social/instagram.png"></a></li>
+                        <li><a target="_blank" href=""><img src="data/images/social/facebook.png"></a></li>
+                        <li><a target="_blank" href=""><img src="data/images/social/twitter.png"></a></li>
+                        <li><a target="_blank" href=""><img src="data/images/social/you_tube.png"></a></li>
+                    </ul>
+                </div>
+        </div>
+    </header>
+
+    <script type="text/javascript">
+    function inscription() {
+        window.location.href = "pages/inscription.php";
+    }
+    function compte(link) {
+        window.location.href = "/pages/mon_compte.php";
+    }
+    </script>
