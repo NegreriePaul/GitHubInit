e92ce26eb13237c077eaebbca19160de648f6997
PtitDoudoux <thomas.dudoux@gmail.com>
2016-12-26 12:12:08 +0100

    Add configuration and helpers files

---
+++ BLOG/conf/conf.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+define("HOST", "localhost");
+define("DS", DIRECTORY_SEPARATOR);
+
+
+$msgError = [
+			1=>"L'adresse email n'est pas correcte",
+			2=>"Le mot de passe doit faire entre 8 et 12 caractères",
+			3=>"Le mot de passe de confirmation ne correspond pas"
+];
