67d6bd1018293978b08136bea3cbd985a06308b7
NegreriePaul <paul.negrerie@gmail.com>
2017-02-13 11:34:45 +0100

    Ajout de dossier inc avec footer et header

---
+++ blog/View/inc/header.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulnegrerie
+ * Date: 13/02/2017
+ * Time: 11:27
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?=\TestProject\Engine\Config::SITE_NAME?></title>
+    <meta name="author" content="Paul Negrerie" />
+    <link rel="stylesheet" href="<?=ROOT_URL?>static/style.css" />
+</head>
+<body>
+<div class="center">
