f2209f9131514e8032bea4a6bba09ebbd2eb3564
NegreriePaul <paul.negrerie@gmail.com>
2017-02-13 11:37:15 +0100

    Ajout de login.php dans les vues

---
+++ blog/View/login.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulnegrerie
+ * Date: 13/02/2017
+ * Time: 11:36
+ */
+
+?>
+<?php require 'inc/header.php' ?>
+<?php require 'inc/msg.php' ?>
+
+    <form action="" method="post">
+
+        <p><label for="email">Email:</label><br />
+            <input type="email" name="email" id="email" required="required" />
+        </p>
+
+        <p><label for="password">Password:</label><br />
+            <input type="password" name="password" id="password" required="required" />
+        </p>
+
+        <p><input type="submit" value="Log In" /></p>
+    </form>
+
+<?php require 'inc/footer.php' ?>
