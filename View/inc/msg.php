e8dc3e790c49b7e96dab3656e906ef55b0b08373
NegreriePaul <paul.negrerie@gmail.com>
2017-02-14 09:56:04 +0100

    include header et footer dans index, msg.php réalisé

--- View/inc/msg.php
+++ View/inc/msg.php
@@ -5,3 +5,17 @@
  * Date: 14/02/2017
  * Time: 09:35
  */
+?>
+<?php if (!empty($this->sErrMsg)): ?>
+
+    <p class="error"><?=$this->sErrMsg?></p>
+
+<?php endif ?>
+
+<?php if (!empty($this->sSuccMsg)): ?>
+
+    <p class="success"><?=$this->sSuccMsg?></p>
+
+<?php endif ?>
+
+
--- View/index.php
+++ View/index.php
@@ -5,3 +5,10 @@
  * Date: 14/02/2017
  * Time: 09:33
  */
+require 'inc/header.php' ?>
+
+<!--view index-->
+
+
+
+ <?php require 'inc/footer.php' ?>
