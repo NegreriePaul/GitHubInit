d06acc5bf3a0fb6450f81f598af1cb714b94911c
NegreriePaul <paul.negrerie@gmail.com>
2017-02-17 11:03:01 +0100

    Ajout fichier a inclure et réalisation des différentes vues

--- View/add_post.php
+++ View/add_post.php
@@ -5,3 +5,22 @@
  * Date: 14/02/2017
  * Time: 09:33
  */
+
+
+require 'inc/header.php';
+require 'inc/msg.php'; ?>
+
+    <form action="" method="post">
+
+        <p><label for="title">Title:</label><br />
+            <input type="text" name="title" id="title" required="required" />
+        </p>
+
+        <p><label for="body">Body:</label><br />
+            <textarea name="body" id="body" rows="5" cols="35" required="required"></textarea>
+        </p>
+
+        <p><input type="submit" name="add_submit" value="Add" /></p>
+    </form>
+
+<?php require 'inc/footer.php'; ?>
--- View/index.php
+++ View/index.php
@@ -5,7 +5,7 @@
  * Date: 14/02/2017
  * Time: 09:33
  */
-require 'inc/footer.php';
+/*require 'inc/header.php';
 session_start();
 require "conf.inc.php";
 //autoloader va inclure les fichiers necessaire seulement eux, rend les require dynamique (inclu les class qu'on veut instancier)
@@ -18,10 +18,30 @@ spl_autoload_register( function ($class) {
     }
 
 
-});
+});*/
 
 
-require 'inc/footer.php';
+require 'inc/header.php' ?>
+
+<?php if (empty($this->oPosts)): ?>
+    <p class="bold">There is no Blog Post.</p>
+    <p><button type="button" onclick="window.location='<?=ROOT_URL?>?p=blog&amp;a=add'" class="bold">Add Your First Blog Post!</button></p>
+<?php else: ?>
+
+    <?php foreach ($this->oPosts as $oPost): ?>
+        <h1><a href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>"><?=htmlspecialchars($oPost->title)?></a></h1>
+
+        <p><?=nl2br(htmlspecialchars(mb_strimwidth($oPost->body, 0, 100, '...')))?></p>
+        <p><a href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>">Want to see more?</a></p>
+        <p class="left small italic">Posted on <?=$oPost->createdDate?></p>
+
+        <?php require 'inc/control_buttons.php' ?>
+        <hr class="clear" /><br />
+    <?php endforeach ?>
+
+<?php endif ?>
+
+<?php require 'inc/footer.php' ?>
 
 
 
